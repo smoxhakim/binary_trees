44dc35d96d749d2f54f89383a3b8b63a3fba2b16
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-06-05 17:50:06 +0100

    Betty_style2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,6 +7,7 @@
  * @value: - is the value to put in the new node
  * Return: a pointer to the new node, or NULL on failure
  */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	if (!parent)
