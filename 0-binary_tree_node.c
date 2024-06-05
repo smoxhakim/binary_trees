eb2f5ffd6ec7b49339f3b37cd0e1ad96bada84c1
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-06-05 12:57:04 +0100

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,15 +11,16 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-    if (newnode == NULL)
-    {
-        return (NULL);
-    }
-    newnode->n = value;
-    newnode->parent = parent;
-    newnode->left = NULL;
-    newnode->right = NULL;
+	binary_tree_t *newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-    return (newnode);
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->n = value;
+	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
+
+	return (newnode);
 }
