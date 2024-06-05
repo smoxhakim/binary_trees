a7d73ec063a801933dcba5ad2807b76b7e1923e6
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-06-05 17:41:49 +0100

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,30 +1,37 @@
 #include "binary_trees.h"
 
-
+/**
+ * binary_tree_insert_left - Creates a new binary tree node at left
+ *
+ * @parent: - is a pointer to the parent node of the node to create
+ * @value: - is the value to put in the new node
+ * Return: a pointer to the new node, or NULL on failure
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    if (!parent)
-    {
-        return (NULL);
-    }
+	if (!parent)
+	{
+		return (NULL);
+	}
 
-    binary_tree_t *newnode = binary_tree_node(parent, value);
+	binary_tree_t *newnode = binary_tree_node(parent, value);
 
-    if (!newnode)
-    {
-        return (NULL);
-    }
+	if (!newnode)
+	{
+		return (NULL);
+	}
 
-    if (parent->left == NULL)
-    {
-        parent->left = newnode;
-    }
-    else
-    {
-        binary_tree_t *old_node = parent->left;
-        parent->left = newnode;
-        newnode->left = old_node;
-        old_node->parent = newnode;
-    }
-    return (newnode);
+	if (parent->left == NULL)
+	{
+		parent->left = newnode;
+	}
+	else
+	{
+		binary_tree_t *old_node = parent->left;
+
+		parent->left = newnode;
+		newnode->left = old_node;
+		old_node->parent = newnode;
+	}
+	return (newnode);
 }
--- binary_trees/1-binary_tree_insert_left.c
+++ binary_trees/1-binary_tree_insert_left.c
@@ -3,28 +3,28 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    if (!parent)
-    {
-        return (NULL);
-    }
+	if (!parent)
+	{
+		return (NULL);
+	}
 
-    binary_tree_t *newnode = binary_tree_node(parent, value);
+	binary_tree_t *newnode = binary_tree_node(parent, value);
 
-    if (!newnode)
-    {
-        return (NULL);
-    }
+	if (!newnode)
+	{
+		return (NULL);
+	}
 
-    if (parent->left == NULL)
-    {
-        parent->left = newnode;
-    }
-    else
-    {
-        binary_tree_t *old_node = parent->left;
-        parent->left = newnode;
-        newnode->left = old_node;
-        old_node->parent = newnode;
-    }
-    return (newnode);
+	if (parent->left == NULL)
+	{
+		parent->left = newnode;
+	}
+	else
+	{
+		binary_tree_t *old_node = parent->left;
+		parent->left = newnode;
+		newnode->left = old_node;
+		old_node->parent = newnode;
+	}
+	return (newnode);
 }
