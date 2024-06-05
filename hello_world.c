bcd226c899e10cc6d439e57568d80102603ef8fd
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-06-05 17:52:11 +0100

    remove_makefile

--- hello_world.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello, World!\n");
-    printf("Nigger\n");
-    return 0;
-}
--- makefile
+++
@@ -1,8 +0,0 @@
-all: compile clean
-
-compile: *c
-	@echo "Compiling..."
-	@gcc *c
-
-clean:
-	rm -f a.out
