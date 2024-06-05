09ad2f4338c9a28cae26e4129e1fbdf4ef34ee68
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-06-05 18:19:50 +0100

    update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,7 +15,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	binary_tree_t *newnode = binary_tree_node(parent, value);
+	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+
+	newnode->parent = parent;
+	newnode->n = value;
+	newnode->left = NULL;
+	newnode->right = NULL;
 
 	if (newnode == NULL)
 	{
