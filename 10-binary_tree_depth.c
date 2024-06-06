4d89d84931382ccfb92fea3b8becd6520c6ee5f6
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-06-06 16:44:24 +0100

    10-binary_tree_depth.c_

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,7 +11,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (!tree || !tree->parent)
 	{
-		return 0;
+		return (0);
 	}
-	return 1 + binary_tree_depth(tree->parent);
+	return (binary_tree_depth(tree->parent) + 1);
 }
