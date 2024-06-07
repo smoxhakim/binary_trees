9f8177d73e347788a86095aae515e097961c67f4
smoxhakim <joulal1994@gmail.com>
2024-06-07 01:31:24 +0100

    15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,5 +9,25 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int left_is_full = 0;
+	int right_is_full = 0;
 
+	if (!tree)
+	{
+		return (0);
+	}
+	if (!tree->left && !tree->right)
+	{
+		return (1);
+	}
+
+	if (!tree->left || !tree->right)
+	{
+		return (0);
+	}
+
+	left_is_full = binary_tree_is_full(tree->left);
+	right_is_full = binary_tree_is_full(tree->right);
+
+	return (left_is_full && right_is_full);
 }
