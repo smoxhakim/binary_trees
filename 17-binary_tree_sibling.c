9119713c1a07bb2a4de2ec92003993d8b729be51
smoxhakim <joulal1994@gmail.com>
2024-06-07 02:08:05 +0100

    17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,5 +10,14 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else if (node->parent->right == node)
+		return (node->parent->left);
+
+	return (NULL);
+
 }
